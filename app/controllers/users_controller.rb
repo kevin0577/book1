b10bbdc2b795ce2f9cb1d5d3d2a6b10bac8fc328
kevin0577 <vagrant@localhost.localdomain>
2020-08-21 14:36:51 +0000

    bookers作成中2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,36 @@
 class UsersController < ApplicationController
+	before_action :autheticate_user!
+	def show
+		@user = User.find(params[:id])
+		@books = @user.books
+		@book = Book.new
+	end
+
+	def index
+		@users = User.all
+		@user = current_user
+		@book = Book.new
+	end
+
+	def edit
+		@user = User.find(params[:id])
+		if @user.id != current_user.id
+			redirect_to user_path(current_user)
+		end
+	end
+
+	def update
+		@user = user.find(params[:id])
+		if @user.update(user_params)
+			flash[:notice] = "successfully"
+			redirect_to user_path(current_user)
+		else
+			render "edit"
+		end
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:user, :introduction, :profile_image)
+	end
 end
